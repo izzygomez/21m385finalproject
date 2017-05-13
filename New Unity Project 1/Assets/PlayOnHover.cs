f84048a3248c6f5201694a4ca07e75857e680f4c
21M385DreamTeam <izzyg@mit.edu>
2017-05-13 11:11:21 -0400

    hiiiiii

---
+++ New Unity Project 1/Assets/PlayOnHover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnHover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Controller")
+        {
+            Debug.Log("controller hit");
+            this.transform.parent.gameObject.SendMessage("Play");
+        }
+    }
+}
